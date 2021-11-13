3e017ed7d216d036780c5ea9969534e9e964ac77
Erik Luebbers <erluebbers@gmail.com>
2021-11-12 17:45:55 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   let form = document.querySelector('#create-task-form')
   form.addEventListener('submit', createNewTask);
 
+function handleDelete (e) {
+  e.target.parentNode.remove()
+}
+
 function createNewTask (e) {
     e.preventDefault()
     let inputItem = document.querySelector('#new-task-description')
-    console.log(inputItem.value)
     let li = document.createElement('li')
-    li.textContent = inputItem.value
+    let btn = document.createElement('button')
+    let dropDown = document.createElement('form')
+    dropDown.innerHTML = `<select> <option value='not important'>not important</option> <option value = 'important'>important</option> <option value='very important'>very important</option> </select>`
+    btn.addEventListener('click', handleDelete)
+    li.textContent = `${inputItem.value} `
+    btn.textContent = 'X'
+    li.appendChild(btn)
+    li.appendChild(dropDown)
     document.querySelector('#tasks').appendChild(li)
     form.reset()
 }
-
 })
